61a2c32fb1902d45efa1dc60bf0652a36f57c999
wizzie-ece <koukopoulos352000@gmail.com>
2020-08-12 23:19:04 +0300

    no.

--- AggelosVisas.py
+++ AggelosVisas.py
@@ -1,1 +1,14 @@
+import discord
 
+token = "NTM1OTkxMTgyMzIxMjU0NTE4.XEJ6sA.9JcLg4mkRKrBt7fdPfZ2WPDgung"
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("ti exoume?"):
+        await message.channel.send("esu tha mas peis")
+    if message.content.startswith("a kala"):
+        await message.channel.send("panta kala")
+
+client.run(token)
